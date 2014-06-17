cd696f25eab0505274e9e81cd4df82f447383ecc
Josh Mervine <jmervine@yp.com>
2014-06-16 23:54:26 -0700

    adding developer notes section

--- doc.go
+++ doc.go
@@ -4,5 +4,9 @@ Package maxcdn is the golang bindings for MaxCDN's REST API.
 This package should be considered beta. The final release will be moved to
 `github.com/maxcdn/go-maxcdn`.
 
+Developer Notes:
+
+ - Currently Pullzones does not support POST requests to Endpoint.Zones.PullBy({zone_id}) as it returns mix types. Use Generic with type assertions instead.
+
 */
 package maxcdn
